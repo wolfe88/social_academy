30931918b23abdc284a8903dd44ead3ab10e734e
Yunus Emre Hançer <yunusemrehancer@gmail.com>
2023-04-08 16:59:44 +0300

    l

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -5,8 +5,64 @@ class EventsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("EventsPage"),
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      height: 61,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(35.0),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 3), blurRadius: 5, color: Colors.grey)
+                ],
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.face,
+                        color: Colors.blueAccent,
+                      ),
+                      onPressed: () {}),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Type Something...",
+                          hintStyle: TextStyle(color: Colors.blueAccent),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.photo_camera, color: Colors.blueAccent),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.attach_file, color: Colors.blueAccent),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(width: 15),
+          Container(
+            padding: const EdgeInsets.all(15.0),
+            decoration:
+                BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
+            child: InkWell(
+              child: Icon(
+                Icons.keyboard_voice,
+                color: Colors.white,
+              ),
+              onLongPress: () {},
+            ),
+          )
+        ],
+      ),
     );
   }
 }
